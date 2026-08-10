45f3a05fab7b7cfba94b882801d5e19bd0b479bb
Ismail shaikh <mohammedrahulshaikh@gmail.com>
2026-08-10 23:07:15 +0530

    test: verify branch protection workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { AppProvider } from "./app/providers/AppProvider";
 import { AppRoutes } from "./routes";
 
+// checking if branch protection works
+
 function App() {
   return (
     <AppProvider>
